358ec6dc7c2cfe8f8ab818df769a551c4b24f042
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-07-19 13:17:10 -0300

    fix makefile sintax

--- Makefile.bat
+++ Makefile.bat
@@ -2,14 +2,14 @@
 
 setlocal
 @REM out put
-set "OutPut=./build/BuildGamegit"
+set "OutPut=.\build\BuildGamegit"
 @REM libs
-set "Vulkan=.\lib\VulkanSDK\lib\*"
-set "GLFW=.\lib\glfw-3.4\lib-static-ucrt\*"
-set "ImGui=./lib/Dear-ImGui/*.cpp"
+set "Vulkan=.\lib\VulkanSDK\lib\* -I.\lib\VulkanSDK\include\"
+set "GLFW=.\lib\glfw-3.4\lib-static-ucrt\* -I.\lib\glfw-3.4\include\"
+set "ImGui=.\lib\Dear-ImGui\*.cpp -I.\lib\Dear-ImGui\"
 @REM all libs together
 set "LIBs=%Vulkan% %GLFW% %ImGui%"
 @REM project files path
-set "ProjectFiles=./src/main.cpp"
+set "ProjectFiles=.\src\main.cpp"
 
 g++ %LIBs% %ProjectFiles% -o %OutPut%
