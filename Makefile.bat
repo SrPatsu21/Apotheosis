e6fd2fe9eb8f23ee043847c55eb3770f15e11017
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-07-23 21:24:48 -0300

    try to fix

--- Makefile.bat
+++ Makefile.bat
@@ -4,9 +4,9 @@ cls
 
 setlocal
 @REM out put
-set "OutPut=.\build\BuildGamegit"
+set "OutPut=.\build\BuildGame"
 @REM libs
-set "Vulkan=.\lib\VulkanSDK\lib\* -I.\lib\VulkanSDK\include\"
+set "Vulkan=-L.\lib\VulkanSDK\lib\*.lib -I.\lib\VulkanSDK\include\"
 set "GLFW=.\lib\glfw-3.4\lib-static-ucrt\* -I.\lib\glfw-3.4\include\"
 set "ImGui=.\lib\Dear-ImGui\*.cpp -I.\lib\Dear-ImGui\"
 @REM all libs together
@@ -16,4 +16,4 @@ set "ProjectFiles=.\src\main.cpp"
 
 @echo on
 
-g++ %LIBs% %ProjectFiles% -o %OutPut%
+x86_64-w64-mingw32-g++ %LIBs% %ProjectFiles% -o %OutPut%
