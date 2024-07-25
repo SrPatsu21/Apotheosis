349e674344853a5ea40c4018e82a8478d7ea508a
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-07-25 20:57:25 -0300

    work for the first time

--- Makefile.bat
+++ Makefile.bat
@@ -6,14 +6,15 @@ setlocal
 @REM out put
 set "OutPut=.\build\BuildGame"
 @REM libs
-set "Vulkan=-L.\lib\VulkanSDK\lib\*.lib -I.\lib\VulkanSDK\include\"
+set "Vulkan=-L.\lib\VulkanSDK\lib\*.lib -I.\lib\VulkanSDK\include\ .\lib\VulkanSDK\lib\*.lib"
 set "GLFW=.\lib\glfw-3.4\lib-static-ucrt\* -I.\lib\glfw-3.4\include\"
 set "ImGui=.\lib\Dear-ImGui\*.cpp -I.\lib\Dear-ImGui\"
 @REM all libs together
-set "LIBs=%Vulkan% %GLFW% %ImGui%"
+set "LIBs=%ImGui% %GLFW% %Vulkan%"
 @REM project files path
 set "ProjectFiles=.\src\main.cpp"
-
+@REM compiler
+set "Compiler=x86_64-w64-mingw32-g++ -std=c++23"
 @echo on
 
-x86_64-w64-mingw32-g++ %LIBs% %ProjectFiles% -o %OutPut%
+%Compiler% -o %OutPut% %ProjectFiles% %LIBs%
