e86bd21262d9638419df486f197b0f243e01a8ff
SrPatsu21 <paulosergiopierdona@gmail.com>
2025-07-24 13:40:18 -0300

    color

--- src/client/swapchain/CommandManager.cpp
+++ src/client/swapchain/CommandManager.cpp
@@ -60,7 +60,7 @@ CommandManager::CommandManager(
         renderPassInfo.renderArea.extent = extent;
 
         VkClearValue clearValues[2];
-        clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
+        clearValues[0].color = {{0.4f, 1.0f, 1.0f, 1.0f}};
         clearValues[1].depthStencil = {1.0f, 0};
 
         renderPassInfo.clearValueCount = 2;
