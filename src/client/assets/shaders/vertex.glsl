9701045f283707b1ced589c15510c4c38280d072
SrPatsu21 <paulosergiopierdona@gmail.com>
2026-01-01 10:52:42 -0300

    UBO vec2

--- src/client/assets/shaders/vertex.glsl
+++ src/client/assets/shaders/vertex.glsl
@@ -6,11 +6,14 @@ layout(location = 1) in vec4 inColor;
 layout(location = 0) out vec4 fragColor;
 
 // Uniform Buffer Object
-layout(set = 0, binding = 0) uniform UBO {
-    mat4 model;
-    mat4 view;
-    mat4 proj;
+layout(std140, set = 0, binding = 0) uniform UniformBufferObject {
+    vec2 foo;          // offset 0
+    vec2 _padding;     // offset 8
+    mat4 model;        // offset 16
+    mat4 view;         // offset 80
+    mat4 proj;         // offset 144
 } ubo;
+
 
 void main() {
     fragColor = inColor;
--- src/client/camera/UniformBufferObject.hpp
+++ src/client/camera/UniformBufferObject.hpp
@@ -3,7 +3,9 @@
 #include <glm/glm.hpp>
 
 struct UniformBufferObject {
-    alignas(16) glm::mat4 model;
-    alignas(16) glm::mat4 view;
-    alignas(16) glm::mat4 proj;
+    glm::vec2 foo;          // offset 0   (8 bytes)
+    glm::vec2 _padding;     // offset 8   (8 bytes)
+    alignas(16) glm::mat4 model; // offset 16  (64 bytes)
+    alignas(16) glm::mat4 view;  // offset 80  (64 bytes)
+    alignas(16) glm::mat4 proj;  // offset 144 (64 bytes)
 };
