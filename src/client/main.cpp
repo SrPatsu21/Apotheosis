7631a32e05a7249681cac6e16a00b7e6cd4c17ab
SrPatsu21 <paulosergiopierdona@gmail.com>
2026-02-01 00:08:12 -0300

    add licensed on code

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,6 @@
+// Copyright © 2026 SrPatsu21
+// Licensed under the Apache License, Version 2.0
+
 #include "./Render.hpp"
 
 int main() {
--- src/common/common.cpp
+++ src/common/common.cpp
@@ -1,1 +1,4 @@
+// Copyright © 2026 SrPatsu21
+// Licensed under the Apache License, Version 2.0
+
 // common.cpp
