fb70fa8832456fd862e5b16a45d55061faa4386e
SrPatsu21 <paulosergiopierdona@gmail.com>
2026-02-08 08:44:43 -0300

    docs VulkanImageUtils

--- src/client/image/VulkanImageUtils.hpp
+++ src/client/image/VulkanImageUtils.hpp
@@ -3,17 +3,6 @@
 #include "../CoreVulkan.hpp"
 #include "../mash/BufferManager.hpp"
 
-/**
-@brief Creates a 2D Vulkan image and allocates & binds its memory.
-@param width Image width in pixels.
-@param height Image height in pixels.
-@param format Format of the image (e.g., depth or color).
-@param tiling Image tiling (optimal or linear).
-@param usage Usage flags describing how the image will be used.
-@param properties Memory property flags (e.g., device local).
-@param image (out) Created VkImage handle.
-@param imageMemory (out) Allocated VkDeviceMemory handle.
-*/
 void createImage(
     VkPhysicalDevice physicalDevice,
     VkDevice device,
@@ -29,16 +18,6 @@ void createImage(
     VkDeviceMemory& imageMemory
 );
 
-/**
-@brief Creates a 2D image view for a given image.
-
-@param image Image handle to create a view for.
-@param format Image format.
-@param aspectFlags Aspect mask (e.g., VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_ASPECT_DEPTH_BIT).
-@return VkImageView The created image view handle.
-
-@throws std::runtime_error if image view creation fails.
- */
 VkImageView createImageView(
     VkDevice device,
     VkImage image,
